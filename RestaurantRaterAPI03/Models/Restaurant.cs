5a95da32963cf8afa81543b31017f2fd18340d97
normabunton <te_nj25@hotmail.com>
2021-01-28 13:22:20 -0500

    new commit

---
+++ RestaurantRaterAPI03/Models/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI03.Models
+{
+    public class Restaurant
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Rating { get; set; }
+
+        
+    }
+}
---
+++ RestaurantRaterAPI03/Models/RestaurantDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI03.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+
+        public RestaurantDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+}
